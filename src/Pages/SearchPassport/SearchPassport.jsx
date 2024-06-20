1c10846fa6766cf2697cfc3daf43ea4230d2e66e
Mmsshakil <softhandshakil@gmail.com>
2024-06-20 06:25:05 +0600

    search page design running

--- src/Pages/SearchPassport/SearchPassport.jsx
+++ src/Pages/SearchPassport/SearchPassport.jsx
@@ -1,9 +1,9 @@
 
 import { useForm } from "react-hook-form";
 import canadaSearch from "../../assets/Banners/canadasearch.jpg"
-import { useContext, useEffect, useState } from "react";
-import { AuthContext } from "../../providers/AuthProvider";
+import { useState } from "react";
 import useAxiosPublic from "../../hooks/useAxiosPublic";
+import approve from "../../assets/icons/approve.png"
 
 const SearchPassport = () => {
 
@@ -24,19 +24,24 @@ const SearchPassport = () => {
 
 
     const onSubmit = (data) => {
-        // console.log(data);
-        // setPassport(data.passportNumber);
-        // console.log(passport);
 
         axiosPublic.get(`/searchPassport/${data.passportNumber}`)
             .then(response => {
-                setUserData(response.data);
+                if (!response.data || response.data.length === 0) {
+                    console.log('Error: No data found');
+                    setUserData(null);
+                } else {
+                    setUserData(response.data);
+                    console.log('data found');
+
+                }
                 setLoading(false);
             })
             .catch(error => {
                 console.log(error);
                 // setPassport(null);
                 setUserData(null);
+                setLoading(false);
             })
 
         if (loading) {
@@ -48,12 +53,11 @@ const SearchPassport = () => {
 
 
 
-
     }
 
     return (
         <div>
-            <div className="hero min-h-20" style={{ backgroundImage: `url(${canadaSearch})` }}>
+            <div className="hero min-h-72 mx-auto" style={{ backgroundImage: `url(${canadaSearch})` }}>
                 <div className="hero-overlay bg-opacity-60"></div>
                 <div className="hero-content text-center text-neutral-content">
                     <div className="max-w-md">
@@ -67,6 +71,73 @@ const SearchPassport = () => {
                 </div>
             </div>
             {/* after */}
+
+            {
+                userData ? <>
+
+                    <div className="card shrink-0 w-full max-w-lg shadow-2xl bg-base-100 rounded-lg pt-2  mx-auto">
+                        <div className="relative">
+                            <figure className="w-52 h-56 mx-auto"><img className="pt-10" src={userData[0].photoUrl} alt="profilepic" /></figure>
+                            <img className="w-28 absolute left-40 -bottom-10 lg:left-48 lg:-bottom-10" src={approve} alt="" />
+                        </div>
+                        <div className="card-body gap-1">
+
+
+                            <table className="table text-center">
+                                {/* head */}
+                                <thead>
+
+                                </thead>
+                                <tbody>
+                                    {/* row 1 */}
+                                    <tr>
+                                        <td className="font-bold">Name</td>
+                                        <td>{userData[0].name}</td>
+                                    </tr>
+                                    {/* row 2 */}
+                                    <tr>
+                                        <td className="font-bold">Passport</td>
+                                        <td>{userData[0].passport}</td>
+                                    </tr>
+                                    {/* row 3 */}
+                                    <tr>
+                                        <td className="font-bold">NID Number</td>
+                                        <td>{userData[0].nid}</td>
+                                    </tr>
+                                    <tr>
+                                        <td className="font-bold">Job Title</td>
+                                        <td>{userData[0]?.jobTitle}</td>
+                                    </tr>
+                                    <tr>
+                                        <td className="font-bold">Company</td>
+                                        <td>{userData[0]?.companyName}</td>
+                                    </tr>
+                                    <tr>
+                                        <td className="font-bold">Father&apos;s Name</td>
+                                        <td>{userData[0]?.fatherName}</td>
+                                    </tr>
+                                    <tr>
+                                        <td className="font-bold">Mother&apos;s Name</td>
+                                        <td>{userData[0]?.motherName}</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+
+
+
+                            
+
+                        </div>
+                    </div>
+
+
+
+
+
+                </> : <>
+                    <h1>data not available</h1>
+                </>
+            }
 
         </div>
     );
