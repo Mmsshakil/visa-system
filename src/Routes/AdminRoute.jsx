5223644f09abdd65cf2602f77390685e0b0872aa
Mmsshakil <softhandshakil@gmail.com>
2024-06-25 04:28:01 +0600

    admin route also added but need to test

---
+++ src/Routes/AdminRoute.jsx
@@ -0,0 +1,27 @@
+import { useContext } from "react";
+import useAdmin from "../hooks/useAdmin";
+import { AuthContext } from "../providers/AuthProvider";
+import { Navigate, useLocation } from "react-router-dom";
+
+
+
+const AdminRoute = (children) => {
+
+    const { user, loading } = useContext(AuthContext);
+    const [isAdmin, isAdminLoading] = useAdmin();
+
+    const location = useLocation();
+    console.log(location);
+
+    if (loading || isAdminLoading) {
+        return <span className="loading loading-spinner text-warning loading-lg"></span>
+    }
+
+    if (user && isAdmin) {
+        return children;
+    }
+
+    return <Navigate to='/login' state={{ from: location }} replace ></Navigate>;
+};
+
+export default AdminRoute;
