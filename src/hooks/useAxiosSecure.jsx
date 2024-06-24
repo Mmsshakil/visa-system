933bfc96da9c5879d69c8ab7c1a69558b59e4faa
Mmsshakil <softhandshakil@gmail.com>
2024-06-25 01:23:09 +0600

    logout if the token does't match

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,42 +1,45 @@
 import axios from "axios";
+import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
-import userAuth from "./userAuth";
+import { AuthContext } from "../providers/AuthProvider";
+
 
 const axiosSecure = axios.create({
     baseURL: 'http://localhost:5000'
-});
+})
 
 const useAxiosSecure = () => {
-    const navigate = useNavigate();
-    const { logOut } = userAuth();
 
-    // request interceptor to add authorization header for every secure call to the api
+    const navigate = useNavigate();
+    const { user, logOut } = useContext(AuthContext);
+
+    // request interceptors to add authorizations header for every secure api
     axiosSecure.interceptors.request.use(function (config) {
         const token = localStorage.getItem('access-token');
-        // console.log('request stopped by interceptors', token);
-
+        console.log('requestred stopped by interceptors', token);
         config.headers.authorization = `Bearer ${token}`;
-
         return config;
     }, function (error) {
+        // do something with request error
         return Promise.reject(error);
-    });
+    })
 
-    // intercepts 401 and 403 status
+
+    // intercept 401 and 403
     axiosSecure.interceptors.response.use(function (response) {
         return response;
-
     }, async (error) => {
-        const status = error.response.status;
-        // console.log('status error in the interceptor', status);
 
-        // for 401 or 403 logout the user and redriect to the login page
+        const status = error.response.status;
+        console.log('status error in the interceptor', status);
         if (status === 401 || status === 403) {
             await logOut();
-            navigate('/login');
+            navigate('/login')
         }
         return Promise.reject(error);
-    });
+    })
+
+
 
     return axiosSecure;
 };
