88754e81f2ecdd961571c753abdefb44145671dc
Mmsshakil <softhandshakil@gmail.com>
2024-07-08 01:56:57 +0600

    payment component added for payment section

--- src/Pages/VisaForm/VisaForm.jsx
+++ src/Pages/VisaForm/VisaForm.jsx
@@ -9,6 +9,7 @@ import bkashicon from '../../assets/payments/Bkash.png'
 import nagadicon from '../../assets/payments/Nagad.png'
 import Swal from "sweetalert2";
 import { useNavigate } from "react-router-dom";
+import Payment from "../../components/Payment";
 
 const VisaForm = () => {
 
@@ -386,108 +387,13 @@ const VisaForm = () => {
                             </div>
                         </div>
 
-                        {/* paypal */}
-                        {
-                            selectedPayment === "Paypal" && <div className="flex flex-col gap-0 items-center justify-center">
-                                <div className="flex gap-0 items-center justify-center">
-                                    <figure className="w-24"><img className="" src={paypalicon} alt="profilepic" /></figure>
-                                    <h1 className="text-sm md:text-lg font-bold">- softhandshakil@yahoo.com</h1>
-                                </div>
+                        <Payment
+                            selectedPayment={selectedPayment}
+                            bkashAmount={"90000"}
+                            othersAmount={"949"}
+                        ></Payment>
 
-                                <div className=" text-sm md:text-base">
-                                    <ol className="list-decimal">
-                                        <li>Sent <span className="font-bold text-blue-700">$949</span> to the PayPal account address.</li>
-                                        <li>Must be sent on <span className="font-bold text-blue-700">&apos;&apos;Friends and Family&apos;&apos;</span> option.</li>
-                                        <li>Enter the <span className="font-bold text-blue-700">TrxID</span> and click the Apply button.</li>
-                                    </ol>
-                                </div>
-                            </div>
-                        }
-
-
-                        {/* bkash */}
-                        {
-                            selectedPayment === "Bkash" && <div className="flex flex-col gap-0 items-center justify-center">
-                                <div className="flex gap-0 items-center justify-center">
-                                    <figure className="w-24"><img className="" src={bkashicon} alt="profilepic" /></figure>
-                                    <h1 className="text-lg font-bold">: +8801712345678</h1>
-                                </div>
-
-                                <div className="text-sm md:text-base">
-                                    <ol className="list-decimal">
-                                        <li>Sent <span className="font-bold text-blue-700">30000/= tk</span> to the BKash account number.</li>
-                                        <li>This is a <span className="font-bold text-blue-700">&apos;&apos;Personal&apos;&apos;</span> account number.</li>
-                                        <li>Enter the <span className="font-bold text-blue-700">TrxID</span> and click the Apply button.</li>
-                                        <li>You can cash in on this number from any agent.</li>
-                                        <li className=" text-red-600">Don&apos;t Cashout or Merchant payment</li>
-                                    </ol>
-                                </div>
-                            </div>
-                        }
-
-
-                        {/* nagad */}
-                        {/* {
-                            selectedPayment === "Nagad" && <div className="flex flex-col gap-0 items-center justify-center">
-                                <div className="flex gap-0 items-center justify-center">
-                                    <figure className="w-24"><img className="" src={nagadicon} alt="profilepic" /></figure>
-                                    <h1 className="text-lg font-bold">: +8801712345678</h1>
-                                </div>
-
-                                <div className="">
-                                    <ol className="list-decimal">
-                                        <li>Sent <span className="font-bold text-blue-700">17100/= tk</span> to the Nagad account number.</li>
-                                        <li>This is a <span className="font-bold text-blue-700">&apos;&apos;Personal&apos;&apos;</span> account number.</li>
-                                        <li>Enter the <span className="font-bold text-blue-700">TrxID</span> and click the Apply button.</li>
-                                        <li className=" text-red-600">Don&apos;t Cashout or Merchant payment</li>
-                                    </ol>
-                                </div>
-                            </div>
-                        } */}
-
-                        {/* apple */}
-                        {
-                            selectedPayment === "ApplePay" && <div className="flex flex-col gap-0 items-center justify-center mt-3">
-                                <div className="flex gap-0 items-center justify-center">
-                                    <figure className="w-20"><img className="" src={appleicon} alt="profilepic" /></figure>
-                                    <div className="text-sm md:text-lg font-bold">
-                                        <h1 >: +8801712345678</h1>
-                                        <h1 >: softhandshakil@yahoo.com</h1>
-                                    </div>
-                                </div>
-
-
-                                <div className="text-sm md:text-base">
-                                    <ol className="list-decimal">
-                                        <li>Sent <span className="font-bold text-blue-700">$949</span> to the Apply Pay Number or Mail.</li>
-                                        <li>Must be sent from <span className="font-bold text-blue-700">&apos;&apos;Apple Cash&apos;&apos;</span>.</li>
-                                        <li>Enter your <span className="font-bold text-blue-700">Apple Pay Number</span> and click the Apply button.</li>
-                                    </ol>
-                                </div>
-                            </div>
-                        }
-
-
-                        {/* usdt */}
-                        {
-                            selectedPayment === "USDT" && <div className="flex flex-col gap-0 items-center justify-center mt-3">
-                                <div className="flex flex-col gap-0 items-center justify-center mb-2">
-                                    <figure className="w-80"><img className="" src={usdticon} alt="profilepic" /></figure>
-                                    <h1 className="text-lg font-bold">Address</h1>
-                                    <h1 className="text-base md:text-lg text-blue-700">TARDRViMtNmkWDGttFcjCMmZaJ6XDeMPNK</h1>
-                                </div>
-
-
-                                <div className="text-sm md:text-base">
-                                    <ol className="list-decimal">
-                                        <li>Sent <span className="font-bold text-blue-700">$949</span> to the USDT address or scan QR code.</li>
-                                        <li>Must be sent by <span className="font-bold text-blue-700">Tron (TRC20)</span> network.</li>
-                                        <li>Check the wallet address carefully.</li>
-                                    </ol>
-                                </div>
-                            </div>
-                        }
-
+                      
 
                     </div>
 
